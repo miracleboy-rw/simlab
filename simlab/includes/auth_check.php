198b2c297e368fcbdf147728178deefc173a181e
miracleboy-rw <ivan.123430119@student.itera.ac.id>
2026-07-10 16:51:07 +0700

    Delete simlab directory

--- simlab/includes/auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../config/functions.php';
-
-if (!isLoggedIn()) {
-    redirect('../auth/login.php');
-}
